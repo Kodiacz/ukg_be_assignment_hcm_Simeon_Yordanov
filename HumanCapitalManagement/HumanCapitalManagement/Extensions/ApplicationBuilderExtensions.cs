f7687ba2b18aebbbffdc307b268839d19a57af92
Simeon Yordanov <SimeonYordanov@deltek.com>
2025-04-05 15:40:17 +0300

    added service and created extension method for application builder

---
+++ HumanCapitalManagement/HumanCapitalManagement/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HumanCapitalManagement.Infrastructure;
+
+namespace HumanCapitalManagement.API.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+	public static async Task SeedDatabase(this IApplicationBuilder app)
+	{
+		using (var scope = app.ApplicationServices.CreateScope())
+		{
+			var dataSeeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+			await dataSeeder.SeedData();
+		}
+	}
+}
--- HumanCapitalManagement/HumanCapitalManagement/Program.cs
+++ HumanCapitalManagement/HumanCapitalManagement/Program.cs
@@ -1,19 +1,26 @@
+using HumanCapitalManagement.API.Extensions;
+using HumanCapitalManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<DataSeeder>();
+
+builder.Services.AddDbContext<HcmDbContext>(options =>
+	options.UseInMemoryDatabase("HcmDatabase"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
+
+	await app.SeedDatabase();
 }
 
 app.UseHttpsRedirection();
