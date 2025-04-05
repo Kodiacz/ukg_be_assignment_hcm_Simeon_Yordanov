8b9a2f24740dae5a9e37dc93c5c0e8f3162565b9
Simeon Yordanov <SimeonYordanov@deltek.com>
2025-04-05 15:42:22 +0300

    created data seeder and base class for json readin and writing

---
+++ HumanCapitalManagement/HumanCapitalManagement.Infrastructure/DataSeeder.cs
@@ -0,0 +1,64 @@
+﻿using HumanCapitalManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HumanCapitalManagement.Infrastructure;
+
+public class DataSeeder : JsonStoreAccessor
+{
+	private readonly HcmDbContext dbContext;
+
+	public DataSeeder(HcmDbContext dbContext)
+	{
+		this.dbContext = dbContext;
+	}
+
+
+	public async Task SeedData()
+	{
+		await this.SeedPersonData();
+		await this.SeedUserData();
+		await this.SeedRoleData();
+		await this.SeedUserRolesData();
+	}
+
+	private async Task SeedPersonData()
+	{
+		var data = LoadFromJson<Person>("persons.json");
+
+		if (!this.dbContext.People.Any())
+			await this.AddToDBContext(data);
+	}
+
+	private async Task SeedUserData()
+	{
+		var data = LoadFromJson<ApplicationUser>("users.json");
+		if (!this.dbContext.ApplicationUsers.Any())
+			await this.AddToDBContext(data);
+	}
+
+	private async Task SeedRoleData()
+	{
+		var data = LoadFromJson<Role>("roles.json");
+		if (!this.dbContext.Roles.Any())
+			await this.AddToDBContext(data);
+	}
+
+	private async Task SeedUserRolesData()
+	{
+		var data = LoadFromJson<UserRoles>("userroles.json");
+		if (!this.dbContext.UserRoles.Any())
+			await this.AddToDBContext(data);
+	}
+
+	private async Task AddToDBContext<T>(List<T> data)
+	{
+		foreach (var item in data)
+		{
+			await this.dbContext.AddAsync(item);
+		}
+
+		await this.dbContext.SaveChangesAsync();
+	}
+}
---
+++ HumanCapitalManagement/HumanCapitalManagement.Infrastructure/JsonStoreAccessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace HumanCapitalManagement.Infrastructure;
+
+public class JsonStoreAccessor
+{
+	protected readonly string basePath = Path.Combine(AppContext.BaseDirectory, "DummyStore");
+
+	protected List<T> LoadFromJson<T>(string fileName)
+	{
+		var fullPath = Path.Combine(this.basePath, fileName);
+
+		if (!File.Exists(fullPath))
+			throw new FileNotFoundException($"Could not find file at path: {fullPath}");
+
+		var jsonData = File.ReadAllText(fullPath);
+		return JsonSerializer.Deserialize<List<T>>(jsonData) ?? new List<T>();
+	}
+
+
+	protected void SaveToJson<T>(string fileName, List<T> data)
+	{
+		var fullPath = Path.Combine(this.basePath, fileName);
+		var json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+		File.WriteAllText(fullPath, json);
+	}
+
+}
