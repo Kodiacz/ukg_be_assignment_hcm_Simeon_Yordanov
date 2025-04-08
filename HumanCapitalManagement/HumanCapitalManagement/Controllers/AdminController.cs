12044157b7d8067ec97f651ffd457e47ea5ad5b8
Simeon Yordanov <SimeonYordanov@deltek.com>
2025-04-08 13:55:20 +0300

    created admin and authentication controller

---
+++ HumanCapitalManagement/HumanCapitalManagement/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿namespace HumanCapitalManagement.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+[Authorize(Roles = "Admin")]
+public class AdminController : ControllerBase
+{
+	private readonly IAdminService adminService;
+
+	public AdminController(IAdminService adminService)
+	{
+		this.adminService = adminService;
+	}
+
+	[HttpGet]
+	[ActionName(nameof(GetAllUsers))]
+	public async Task<IActionResult> GetAllUsers([FromQuery] UserInputDto inputDto)
+	{
+		var users = await this.adminService.GetAllUsers(inputDto);
+		return Ok(users);
+	}
+
+	[HttpPatch]
+
+	public async Task<IActionResult> UpdateUserRole(UserRoleUpdateInputDto input)
+	{
+		await adminService.UpdateUserRoleAsync(input);
+		return Ok();
+	}
+
+}
---
+++ HumanCapitalManagement/HumanCapitalManagement/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿namespace HumanCapitalManagement.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class AuthenticationController : ControllerBase
+{
+	private readonly IAuthService authService;
+	private readonly IJwtProvider jwtProvider;
+
+	public AuthenticationController(IAuthService authService, IJwtProvider jwtProvider)
+	{
+		this.authService = authService;
+		this.jwtProvider = jwtProvider;
+	}
+
+	[HttpPost]
+	[AllowAnonymous]
+	[ActionName(nameof(Login))]
+	public async Task<IActionResult> Login(LoginInputData loginData)
+	{
+		var getUser = await this.authService.LoginUserAsync(loginData);
+		var token = this.jwtProvider.GenerateToken(getUser);
+		return Ok(token);
+	}
+
+	[HttpPost]
+	//[Authorize(Roles = "Admin")]
+	[ActionName(nameof(Register))]
+	public async Task<IActionResult> Register(RegisterInputData registerData)
+	{
+		await this.authService.RegisterUserAsync(registerData);
+		return Ok();
+	}
+}
