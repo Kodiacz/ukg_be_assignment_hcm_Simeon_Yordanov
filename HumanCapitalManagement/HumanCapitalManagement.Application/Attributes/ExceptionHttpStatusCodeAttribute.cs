83c263334c3e022c5cb08508ed61d300db17a0c0
Simeon Yordanov <SimeonYordanov@deltek.com>
2025-04-08 13:58:47 +0300

    created new attribute

---
+++ HumanCapitalManagement/HumanCapitalManagement.Application/Attributes/ExceptionHttpStatusCodeAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace HumanCapitalManagement.Application.Attributes;
+
+public class ExceptionHttpStatusCodeAttribute : Attribute
+{
+	public HttpStatusCode HttpStatusCode { get; }
+
+	public ExceptionHttpStatusCodeAttribute(HttpStatusCode httpStatusCode)
+	{
+		HttpStatusCode = httpStatusCode;
+	}
+}
+
